d594e00a425d08f684a65194309b03e59eb73eb7
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-03-31 22:03:27 +0800

    .

--- nn6000v2/scripts/packages.sh
+++ nn6000v2/scripts/packages.sh
@@ -16,7 +16,7 @@ remove_unwanted_packages() {
     )
     local small8_packages=(
         "ppp" "firewall" "dae" "daed" "daed-next" "libnftnl" "nftables" "dnsmasq" "luci-app-alist"
-        "alist" "opkg" "smartdns" "luci-app-smartdns" "easytier" "webd" "luci-app-natmap"
+        "alist" "opkg" "smartdns" "luci-app-smartdns" "easytier" "luci-app-natmap"
     )
 
     for pkg in "${luci_packages[@]}"; do
