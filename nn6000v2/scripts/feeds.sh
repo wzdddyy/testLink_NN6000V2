0fd7e94e5ac250ea39c129d520273d0e11755a90
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-04-07 17:41:54 +0800

    更新 feeds.sh

--- nn6000v2/scripts/feeds.sh
+++ nn6000v2/scripts/feeds.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/env bash
-
 update_feeds() {
     local FEEDS_PATH="$BUILD_DIR/$FEEDS_CONF"
     if [[ -f "$BUILD_DIR/feeds.conf" ]]; then
         FEEDS_PATH="$BUILD_DIR/feeds.conf"
     fi
-
-    sed -i '/^[[:space:]]*$/d' "$FEEDS_PATH"
+    sed -i '/^#/d' "$FEEDS_PATH"
     sed -i '/packages_ext/d' "$FEEDS_PATH"
-    printf '\n' >> "$FEEDS_PATH"
 
-    # 添加 openwrt-packages 源
-    echo "src-git openwrt-packages https://github.com/kenzok8/openwrt-packages" >>"$FEEDS_PATH"
+    if ! grep -q "openwrt-packages" "$FEEDS_PATH"; then
+        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+        echo "src-git openwrt-packages https://github.com/kenzok8/openwrt-packages" >>"$FEEDS_PATH"
+    fi
 
-    # 添加 passwall-packages 源
-    echo "src-git passwall-packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages" >>"$FEEDS_PATH"
+    if ! grep -q "openwrt-passwall" "$FEEDS_PATH"; then
+        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+        echo "src-git passwall-packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages" >>"$FEEDS_PATH"
+    fi
 
-    # 再清理一遍空行
-    sed -i '/^[[:space:]]*$/d' "$FEEDS_PATH"
-
-    # 修复 bpf.mk
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
         touch "$BUILD_DIR/include/bpf.mk"
     fi
 
-    # 执行更新
-    cd "$BUILD_DIR" || exit 1
     ./scripts/feeds update -a
-    cd - >/dev/null || exit 1
 }
 
 install_feeds() {
