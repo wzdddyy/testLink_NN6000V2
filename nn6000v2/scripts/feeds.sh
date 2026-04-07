cfc8c5f3a13a66106d6327a9cdc7ab39b4e2ee59
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-04-07 16:27:39 +0800

    更新 feeds.sh

--- nn6000v2/scripts/feeds.sh
+++ nn6000v2/scripts/feeds.sh
@@ -4,16 +4,21 @@ update_feeds() {
     if [[ -f "$BUILD_DIR/feeds.conf" ]]; then
         FEEDS_PATH="$BUILD_DIR/feeds.conf"
     fi
+    # 删除注释行
     sed -i '/^#/d' "$FEEDS_PATH"
-    sed -i '/packages_ext/d' "$FEEDS_PATH"
+    
+    # 删除包含 packages_ext 的行（如果存在）
+    grep -q "packages_ext" "$FEEDS_PATH" && sed -i '/packages_ext/d' "$FEEDS_PATH"
 
+    # 添加 openwrt-packages 源
     if ! grep -q "openwrt-packages" "$FEEDS_PATH"; then
-        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+        echo "" >>"$FEEDS_PATH"
         echo "src-git openwrt-packages https://github.com/kenzok8/openwrt-packages" >>"$FEEDS_PATH"
     fi
 
+    # 添加 passwall-packages 源
     if ! grep -q "passwall-packages" "$FEEDS_PATH"; then
-        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+        echo "" >>"$FEEDS_PATH"
         echo "src-git passwall-packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages" >>"$FEEDS_PATH"
     fi
 
