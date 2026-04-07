1ac1cbd934fcbdc6666947732c309657fb3a123d
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-04-07 20:04:43 +0800

    更新 build.sh

--- nn6000v2/scripts/build.sh
+++ nn6000v2/scripts/build.sh
@@ -92,13 +92,6 @@ modify_kernel_size
 cd "$BASE_PATH/../$BUILD_DIR"
 make defconfig
 
-if grep -qE "^CONFIG_TARGET_x86_64=y" "$CONFIG_FILE"; then
-    DISTFEEDS_PATH="$BASE_PATH/../$BUILD_DIR/package/emortal/default-settings/files/99-distfeeds.conf"
-    if [ -d "${DISTFEEDS_PATH%/*}" ] && [ -f "$DISTFEEDS_PATH" ]; then
-        sed -i 's/aarch64_cortex-a53/x86_64/g' "$DISTFEEDS_PATH"
-    fi
-fi
-
 if [[ $Build_Mod == "debug" ]]; then
     exit 0
 fi
