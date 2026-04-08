ac693e321b58e97996c1de0d732bc8f09bfd7849
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-04-08 11:12:46 +0800

    更新 system.sh

--- nn6000v2/scripts/system.sh
+++ nn6000v2/scripts/system.sh
@@ -226,16 +226,16 @@ fix_rust_compile_error() {
 
 update_nginx_ubus_module() {
     local makefile_path="$BUILD_DIR/feeds/packages/net/nginx/Makefile"
-    local source_date="2024-03-02"
-    local source_version="564fa3e9c2b04ea298ea659b793480415da26415"
-    local mirror_hash="92c9ab94d88a2fe8d7d1e8a15d15cfc4d529fdc357ed96d22b65d5da3dd24d7f"
-    local pkg_version="1.26.3"
+    local source_date="2026-03-24"
+    local source_version="release-1.28.3"
+    local mirror_hash="2c96a946bfb0882a21744ed429770a2123ae1828c7c48665092993ddee91a918"
+    local pkg_version="1.28.3"
 
     if [ -f "$makefile_path" ]; then
-        sed -i "s/SOURCE_DATE:=2020-09-06/SOURCE_DATE:=$source_date/g" "$makefile_path"
-        sed -i "s/SOURCE_VERSION:=b2d7260dcb428b2fb65540edb28d7538602b4a26/SOURCE_VERSION:=$source_version/g" "$makefile_path"
-        sed -i "s/MIRROR_HASH:=515bb9d355ad80916f594046a45c190a68fb6554d6795a54ca15cab8bdd12fda/MIRROR_HASH:=$mirror_hash/g" "$makefile_path"
-        sed -i "s/PKG_VERSION:=.*\s/PKG_VERSION:=$pkg_version/g" "$makefile_path"
+        sed -i "s/SOURCE_DATE:=2024-03-02/SOURCE_DATE:=$source_date/g" "$makefile_path"
+        sed -i "s/SOURCE_VERSION:=564fa3e9c2b04ea298ea659b793480415da26415/SOURCE_VERSION:=$source_version/g" "$makefile_path"
+        sed -i "s/MIRROR_HASH:=92c9ab94d88a2fe8d7d1e8a15d15cfc4d529fdc357ed96d22b65d5da3dd24d7f/MIRROR_HASH:=$mirror_hash/g" "$makefile_path"
+        sed -i "s/PKG_VERSION:=1.26.3/PKG_VERSION:=$pkg_version/g" "$makefile_path"
         echo "已更新 nginx-mod-ubus 模块的 SOURCE_DATE, SOURCE_VERSION, MIRROR_HASH 和 PKG_VERSION。"
     else
         echo "错误：未找到 $makefile_path 文件，无法更新 nginx-mod-ubus 模块。" >&2
