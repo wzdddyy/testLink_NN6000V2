73699e37056b4130af93772dd263fde7e4a406df
wzdddyy <89689685+wzdddyy@users.noreply.github.com>
2026-04-08 13:22:00 +0800

    更新 update.sh

--- nn6000v2/scripts/update.sh
+++ nn6000v2/scripts/update.sh
@@ -36,13 +36,10 @@ source "$SCRIPT_DIR/docker.sh"
 
 
 main() {
-    # 1. 环境准备
     clone_repo
     clean_up
-    reset_feeds_conf 
-    # 2. Feeds 更新
-    update_feeds   
-    # 3. 独立包安装
+    reset_feeds_conf
+    update_feeds
     install_timecontrol
     install_quickfile
     install_lucky
@@ -52,9 +49,6 @@ main() {
     install_passwall2
     install_easytier
     install_oaf
-    # 4. 统一安装
-    install_feeds
-    # 5. 系统配置
     remove_tweaked_packages
     fix_default_set
     fix_miniupnpd
@@ -72,8 +66,7 @@ main() {
     update_nss_diag
     fix_compile_coremark
     update_dnsmasq_conf
-    add_backup_info_to_sysupgrade   
-    # 6. 构建配置阶段
+    add_backup_info_to_sysupgrade
     set_nginx_default_config
     update_uwsgi_limit_as
     update_nginx_ubus_module
@@ -82,10 +75,11 @@ main() {
     fix_kconfig_recursive_dependency
     update_docker_stack
     update_script_priority
+    install_feeds
     fix_openssl_ktls
     fix_opkg_check
-    fix_quectel_cm   
-    # 7. PBR 配置阶段
+    fix_quectel_cm
+    install_feeds
     install_pbr_isp
     fix_pbr_ip_forward
 }
